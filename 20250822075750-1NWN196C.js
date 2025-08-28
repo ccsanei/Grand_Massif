65010ede4a5feec4da0cb375eea7e3d62d1fa7c4
ccsanei <charly@thesanei.com>
2025-08-28 18:38:42 +0200

    Add files via upload

---
+++ 20250822075750-1NWN196C.js
@@ -0,0 +1,33 @@
+window.botpress.init({
+  "botId": "dc0b4709-2cbc-45fb-b09a-1fa3dacac30a",
+  "configuration": {
+    "version": "v1",
+    "botName": "Grand Massif domaines skiables",
+    "botAvatar": "https://files.bpcontent.cloud/2025/08/26/11/20250826111349-76NXJXH2.jpeg",
+    "botDescription": "Je peux vous aider pour toute question lié au domaine skiable de Grand Massif",
+    "fabImage": "https://files.bpcontent.cloud/2025/08/26/10/20250826104205-8YK3P2YF.png",
+    "website": {
+      "title": " https://www.grand-massif.com/",
+      "link": " https://www.grand-massif.com/"
+    },
+    "email": {
+      "title": "contact@grand-massif.com",
+      "link": "contact@grand-massif.com"
+    },
+    "phone": {
+      "title": "+33 4 50 90 40 00",
+      "link": "+33 4 50 90 40 00"
+    },
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#54aea5",
+    "variant": "solid",
+    "headerVariant": "solid",
+    "themeMode": "light",
+    "fontFamily": "inter",
+    "radius": 4,
+    "feedbackEnabled": true,
+    "footer": "[⚡ by The Sane Intention](https://fr.linkedin.com/company/the-sane-intention)"
+  },
+  "clientId": "717aa1dd-244f-4625-8a56-0df47120bcc3"
+});
